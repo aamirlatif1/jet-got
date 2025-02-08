8dd538cbabed7599d3cd95117aa22f65dda4b045
Aamir Latif <aamirlatif@gmail.com>
2025-02-08 19:40:39 +0100

    add initial struture of project

---
+++ src/main/java/com/jet/JetGotApplication.java
@@ -0,0 +1,13 @@
+package com.jet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetGotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JetGotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jet/JetGotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetGotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
